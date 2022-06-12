e7af23725cd799604d2afd8f5897319709ac9908
Adrian Gomes <adrianhgomes@gmail.com>
2022-06-12 17:07:26 -0400

    diminish hit box once hit

--- js/class.js
+++ js/class.js
@@ -320,6 +320,8 @@ class Fire extends Enemy {
             this.has_hit_player = true
             is_death(Player.State.TOASTER_DEATH, true)
             this.width = 0
+            this.hit_box.width = 0
+            this.hit_box.height = 0
         }
 
         // TODO: improve hit wall interaction
@@ -328,6 +330,8 @@ class Fire extends Enemy {
         if (is_on_platform(this)) {
             this.has_hit_wall = true
             this.width = 0
+            this.hit_box.width = 0
+            this.hit_box.height = 0
         }
 
         return this
@@ -364,6 +368,8 @@ class Butter extends Actor {
 
         if (this.width < 0) {
             this.width = 0
+            this.hit_box.width = 0
+            this.hit_box.height = 0
             return this
         }
 
